507148745fb16fec97694186d3b49e2b0f2cefba
V.S.VINODINI <86525636+Vinodini01@users.noreply.github.com>
2021-06-26 20:41:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js-7Days </h1>
+      <p>
+        The basics of Javascript and React.js will taught in the bootcamp. A
+        project will be given at the final day
+      </p>
+    </div>
+  );
+}
+
+export default Note;
